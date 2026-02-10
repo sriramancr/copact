84c2ff0720ab182b267354fb5f12196179972df1
sriramancr <81683167+sriramancr@users.noreply.github.com>
2026-02-10 08:20:54 +0530

    Add files via upload

--- contactus.py
+++ contactus.py
@@ -9,6 +9,7 @@ import streamlit as st
 
 
 def main():
+    
     msg = f''' 
         We would love to hear from you. We welcome you to provide your feedback on Copact and help us with further developments. \n
 
@@ -17,6 +18,28 @@ def main():
         📩         drsunder2025@outlook.com
     '''
 
+    st.subheader("Your Voice Matters")
     st.write(msg)
+    
+    st.divider()
+    
+    msg = f''' 
+    Copyright Notice and Proprietary Rights Statement
+    © 2026 CoPact. All rights reserved.
+
+    CoPact is a copyrighted product and the exclusive intellectual property of CoPact. \n
+    All content, designs, source code, documentation, trademarks, service marks, logos, methodologies, workflows, and related materials associated with CoPact are protected under applicable copyright, trademark, and intellectual property laws.
+
+    No part of CoPact may be copied, reproduced, modified, distributed, transmitted, displayed, published, licensed, or used in any form or by any means—electronic, mechanical, photocopying, recording, or otherwise—without the prior written permission of CoPact, except as expressly permitted by applicable law or under a valid licensing agreement.
+
+    Unauthorized use, duplication, or redistribution of CoPact or any of its components may result in civil and/or criminal liability under applicable laws.
+
+    CoPact is provided subject to the terms and conditions outlined in the applicable license agreement. All third-party trademarks, product names, and logos referenced, if any, are the property of their respective owners
+
+    '''
+    
+    st.subheader("Copyright Notice")
+    st.caption(msg)
+
     
     
